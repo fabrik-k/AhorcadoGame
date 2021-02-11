04a5c378f96f7d2ed32a2e25ade8386392702bd5
fabrik-k <aquinatoa@cifpfbmoll.eu>
2021-02-11 13:30:02 +0100

    Before Each Se ha creado el medoto inicialitzarPartida para poder hacer el test @BeforeEach TDD, donde el primer parametro de este metodo es un string "paraula" y el segundo parámetro del metodo es un int "4"

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -1,4 +1,30 @@
 package com.jaume.penjat;
 
 public class Tauler {
+    private char[] ParaulaSecreta;
+
+
+    private int intents;
+
+    public void inicialitzarPartida(String paraula, int intents){
+        setParaulaSecreta(paraula.toCharArray());
+        setIntents(intents);
+    }
+
+    public char[] getParaulaSecreta() {
+        return ParaulaSecreta;
+    }
+
+    public void setParaulaSecreta(char[] paraulaSecreta) {
+        ParaulaSecreta = paraulaSecreta;
+    }
+
+
+    public int getIntents() {
+        return intents;
+    }
+
+    public void setIntents(int intents) {
+        this.intents = intents;
+    }
 }
